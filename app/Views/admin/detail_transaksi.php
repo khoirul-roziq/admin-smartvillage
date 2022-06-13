0909acbaed76dc63dbad0aaac8b50422dc426feb
AhmadSyafarudin <ahmadsyafar123@gmail.com>
2022-06-13 12:30:56 +0700

    Detail Transaksi

--- app/Views/admin/detail_transaksi.php
+++ app/Views/admin/detail_transaksi.php
@@ -1,148 +1,161 @@
-        <main class="h-full pb-16 overflow-y-auto">
-          <div class="container grid px-6 mx-auto">
+<main class="h-full pb-16 overflow-y-auto">
+  <div class="container grid px-6 mx-auto">
 
-            <!-- With actions -->
-            <h4 class="mb-4 my-6 text-lg font-semibold text-gray-600 dark:text-gray-300">
-              Detail Transaksi
-            </h4>
+    <!-- With actions -->
+    <h4 class="mb-4 my-6 text-lg font-semibold text-gray-600 dark:text-gray-300">
+      Detail Transaksi
+    </h4>
 
-            <div class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs">
-              <h4 class="mb-4 font-semibold">
-                Colored card
-              </h4>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Fuga, cum commodi a omnis numquam quod? Totam exercitationem
-                quos hic ipsam at qui cum numquam, sed amet ratione! Ratione,
-                nihil dolorum.
-              </p>
-            </div>
-
-            <div class="w-full overflow-hidden rounded-lg shadow-xs">
-              <div class="w-full overflow-x-auto">
-                <table class="w-full whitespace-no-wrap" id="dataTable">
-                  <thead>
-                    <tr class=" text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                      <th class="px-4 py-3">Client</th>
-                      <th class="px-4 py-3">Amount</th>
-                      <th class="px-4 py-3">Status</th>
-                      <th class="px-4 py-3">Tanggal</th>
-                      <th class="px-4 py-3">Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
-                    <?php
-                    foreach ($transaksi as $index => $data) {
-                      // $data2 = $total[$index];
-                    ?>
-                      <tr class="text-gray-700 dark:text-gray-400">
-                        <td class="px-4 py-3">
-                          <div class="flex items-center text-sm">
-                            <!-- Avatar with inset shadow -->
-                            <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                              <img class="object-cover w-full h-full rounded-full" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ" alt="" loading="lazy" />
-                              <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                            </div>
-                            <div>
-                              <p class="font-semibold"><?= esc($data["nama_pelanggan"]) ?></p>
-                              <p class="text-xs text-gray-600 dark:text-gray-400">
-                                <?= esc($data["nama_desa"]) ?>
-                              </p>
-                            </div>
-                          </div>
-                        </td>
-                        <td class="px-4 py-3 text-sm">
-                          <?= "Rp" . esc(number_format($data["total"], 2, ',', '.')); ?>
-                        </td>
-                        <?php if ($data["status"] == 1) { ?>
-                          <td class="px-4 py-3 text-xs">
-                            <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                              Approved
-                            </span>
-                          </td>
-                        <?php } else if ($data["status"] == 2) { ?>
-                          <td class="px-4 py-3 text-xs">
-                            <span class="px-2 py-1 font-semibold leading-tight text-orange-700 bg-orange-100 rounded-full dark:text-white dark:bg-orange-600">
-                              Pending
-                            </span>
-                          </td>
-                        <?php } else { ?>
-                          <td class="px-4 py-3 text-xs">
-                            <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                              Denied
-                            </span>
-                          </td>
-                        <?php } ?>
-                        <td class="px-4 py-3 text-sm">
-                          <?= esc($data["tanggal"]) ?>
-                        </td>
-                        <td class="px-4 py-3">
-                          <div class="flex items-center space-x-4 text-sm">
-                            <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
-                              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
-                              </svg>
-                            </button>
-                            <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
-                              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                              </svg>
-                            </button>
-                            <button onclick="window.location.href=`<?= base_url('/transaksi/detail') . '/' . $data['id_pelanggan'] . '/' . $data['tanggal'] ?>`" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Detail">
-                              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
-                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-                              </svg>
-                            </button>
-                            <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Print">
-                              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
-                                <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
-                                <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
-                              </svg>
-                            </button>
-                          </div>
-                        </td>
-                      </tr>
-                    <?php }; ?>
-
-                  </tbody>
-                </table>
-              </div>
-
-            </div>
-          </div>
-        </main>
+    <div class="min-w-full mb-8 p-4 text-white bg-purple-600 rounded-lg shadow-xs">
+      <?php
+      $nama = $transaksi[0]["nama_pelanggan"];
+      $alamat = $transaksi[0]["alamat"];
+      $telp = $transaksi[0]["no_telp"];
+      ?>
+      <div class="flex items-center  text-sm">
+        <!-- Avatar with inset shadow -->
+        <div class="relative hidden mr-3 md:block">
+          <img class="object-cover" style="height:200px;" src="<?= base_url('/assets/img/avatar.png') ?>" alt="" loading="lazy" />
+          <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
         </div>
+        <div style="margin-left: 30px;">
+          <p class="text-lg font-semibold te">Nama Pelanggan </p>
+          <p class="text-base text-white-600 dark:text-gray-400 mb-4"><?= $nama ?></p>
+          <p class="text-lg font-semibold te">Alamat </p>
+          <p class="text-base text-white-600 dark:text-gray-400 mb-4"><?= $alamat ?></p>
+          <p class="text-lg font-semibold te">Kontak </p>
+          <p class="text-base text-white-600 dark:text-gray-400"><?= $telp ?></p>
         </div>
-        <script>
-          $(document).ready(function() {
-            $('#dataTable').DataTable({
-              "language": {
-                "decimal": "",
-                "emptyTable": "NO DATA FOUND",
-                "info": "SHOWING _START_ - _END_ OF _TOTAL_",
-                "infoEmpty": "EMPTY",
-                "infoFiltered": "(filtered from _MAX_ total entries)",
-                "infoPostFix": "",
-                "thousands": ",",
-                "lengthMenu": "Show _MENU_ entries",
-                "loadingRecords": "Loading...",
-                "processing": "",
-                "search": "Search:",
-                "zeroRecords": "No matching records found",
-                "paginate": {
-                  "first": "First",
-                  "last": "Last",
-                  "next": "Next",
-                  "previous": "Previous"
-                },
-                "aria": {
-                  "sortAscending": ": activate to sort column ascending",
-                  "sortDescending": ": activate to sort column descending"
-                }
-              }
-            })
-          });
-        </script>
-        </body>
+      </div>
+    </div>
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+      <div class="w-full overflow-x-auto">
+        <table class="w-full whitespace-no-wrap" id="dataTable">
+          <thead>
+            <tr class=" text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+              <th class="px-4 py-3">Barang</th>
+              <th class="px-4 py-3">Harga Barang</th>
+              <th class="px-4 py-3">Quantity</th>
+              <th class="px-4 py-3">Layanan</th>
+              <th class="px-4 py-3">Harga Layanan</th>
+              <th class="px-4 py-3">Total Harga</th>
+              <th class="px-4 py-3">Tanggal</th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+            <?php
+            foreach ($transaksi as $index => $data) {
+              // $data2 = $total[$index];
+            ?>
+              <tr class="text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3 text-sm">
+                  <?php if (esc($data["nama_barang"] == NULL)) {
+                    echo "-";
+                  } else {
+                    echo esc($data["nama_barang"]);
+                  } ?>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <?php if (esc($data["nama_barang"] == NULL)) {
+                    echo "-";
+                  } else {
+                    echo "Rp" . esc(number_format($data["harga_barang"], 2, ',', '.'));
+                  } ?>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <?php if (esc($data["nama_barang"] == NULL)) {
+                    echo "-";
+                  } else {
+                    echo esc($data["qty"]);
+                  } ?>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <?php if (esc($data["nama_layanan"] == NULL)) {
+                    echo "-";
+                  } else {
+                    echo esc($data["nama_layanan"]);
+                  } ?>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <?php if (esc($data["nama_layanan"] == NULL)) {
+                    echo "-";
+                  } else {
+                    echo  "Rp" . esc(number_format($data["harga_layanan"], 2, ',', '.'));
+                  } ?>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <?= "Rp" . esc(number_format($data["total"], 2, ',', '.')); ?>
+                </td>
+                <td class="px-4 py-3 text-sm">
+                  <?= esc($data["tanggal"]) ?>
+                </td>
+                <!-- <td class="px-4 py-3">
+                  <div class="flex items-center space-x-4 text-sm">
+                    <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+                      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                      </svg>
+                    </button>
+                    <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+                      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                      </svg>
+                    </button>
+                    <button onclick="window.location.href=`<?= base_url('/transaksi/detail') . '/' . $data['id_pelanggan'] . '/' . $data['tanggal'] ?>`" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Detail">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                      </svg>
+                    </button>
+                    <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Print">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+                        <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+                        <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                      </svg>
+                    </button>
+                  </div>
+                </td> -->
+              </tr>
+            <?php }; ?>
 
-        </html>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+  </div>
+</main>
+</div>
+</div>
+<script>
+  $(document).ready(function() {
+    $('#dataTable').DataTable({
+      "language": {
+        "decimal": "",
+        "emptyTable": "NO DATA FOUND",
+        "info": "SHOWING _START_ - _END_ OF _TOTAL_",
+        "infoEmpty": "EMPTY",
+        "infoFiltered": "(filtered from _MAX_ total entries)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Show _MENU_ entries",
+        "loadingRecords": "Loading...",
+        "processing": "",
+        "search": "Search:",
+        "zeroRecords": "No matching records found",
+        "paginate": {
+          "first": "First",
+          "last": "Last",
+          "next": "Next",
+          "previous": "Previous"
+        },
+        "aria": {
+          "sortAscending": ": activate to sort column ascending",
+          "sortDescending": ": activate to sort column descending"
+        }
+      }
+    })
+  });
+</script>
+</body>
+
+</html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,21 +7,21 @@
   <title><?= esc($title) ?></title>
 
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="../assets/css/tailwind.output.css" />
-  <link rel="stylesheet" href="../assets/css/validation.css" />
+  <link rel="stylesheet" href="<?= base_url('/assets/css/tailwind.output.css') ?>" />
+  <link rel="stylesheet" href="<?= base_url('/assets/css/validation.css') ?>" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
-  <link rel="stylesheet" type="text/css" href="assets/css/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('/assets/css/jquery.dataTables.css') ?>">
 
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
-  <script src="/assets/js/charts-lines.js" defer></script>
-  <script src="/assets/js/charts-pie.js" defer></script>
-  <script src="../assets/js/init-alpine.js"></script>
-  <script src="../assets/js/jquery-3.6.0.js"></script>
-  <script src="../assets/js/jquery-validate-1.19.3.min.js"></script>
-  <script src="../assets/js/menu.js"></script>
-  <script type="text/javascript" charset="utf8" src="assets/js/jquery.dataTables.js"></script>
+  <script src="<?= base_url('/assets/js/charts-lines.js') ?>" defer></script>
+  <script src="<?= base_url('/assets/js/charts-pie.js') ?>" defer></script>
+  <script src="<?= base_url('/assets/js/init-alpine.js') ?>"></script>
+  <script src="<?= base_url('/assets/js/jquery-3.6.0.js') ?>"></script>
+  <script src="<?= base_url('/assets/js/jquery-validate-1.19.3.min.js') ?>"></script>
+  <script src="<?= base_url('/assets/js/menu.js') ?>"></script>
+  <script type="text/javascript" charset="utf8" src="<?= base_url('/assets/js/jquery.dataTables.js') ?>"></script>
   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script> -->
 </head>
