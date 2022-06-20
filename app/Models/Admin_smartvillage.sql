240feb8af986b7199055f4ffbc0a8965e79450c3
AhmadSyafarudin <ahmadsyafar123@gmail.com>
2022-06-20 10:53:37 +0700

    Update SQL

--- app/Models/Admin_smartvillage.sql
+++ app/Models/Admin_smartvillage.sql
@@ -18,28 +18,48 @@ DROP DATABASE IF EXISTS `admin_smartvillage`;
 CREATE DATABASE IF NOT EXISTS `admin_smartvillage` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `admin_smartvillage`;
 
+-- Dumping structure for table admin_smartvillage.barang_order
+CREATE TABLE IF NOT EXISTS `barang_order` (
+  `id_barang` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `kode_barang` char(11) COLLATE utf8_bin DEFAULT NULL,
+  `nama_barang` mediumtext COLLATE utf8_bin,
+  `harga_barang` int(10) unsigned DEFAULT '0',
+  `qty` int(11) DEFAULT NULL,
+  `id_transaksi` varchar(64) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_barang`),
+  KEY `FK__data_transaksi` (`id_transaksi`),
+  CONSTRAINT `FK__data_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `data_transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumping data for table admin_smartvillage.barang_order: ~1 rows (approximately)
+DELETE FROM `barang_order`;
+/*!40000 ALTER TABLE `barang_order` DISABLE KEYS */;
+INSERT INTO `barang_order` (`id_barang`, `kode_barang`, `nama_barang`, `harga_barang`, `qty`, `id_transaksi`) VALUES
+	('62afed9b227d9', 'B001', 'Asus ROG ', 30000000, 1, '62afed9b1459a'),
+	('62afeebf699a9', 'B002', 'Aqua', 2500, 2, '62afeebf68cfe');
+/*!40000 ALTER TABLE `barang_order` ENABLE KEYS */;
+
 -- Dumping structure for table admin_smartvillage.data_barang
 CREATE TABLE IF NOT EXISTS `data_barang` (
-  `kode_barang` char(5) COLLATE utf8_bin NOT NULL,
+  `kode_barang` char(10) COLLATE utf8_bin NOT NULL,
   `nama_barang` varchar(30) COLLATE utf8_bin NOT NULL,
-  `harga_barang` int(11) NOT NULL,
+  `harga_barang` int(11) unsigned NOT NULL,
   PRIMARY KEY (`kode_barang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table admin_smartvillage.data_barang: ~2 rows (approximately)
+-- Dumping data for table admin_smartvillage.data_barang: ~0 rows (approximately)
 DELETE FROM `data_barang`;
 /*!40000 ALTER TABLE `data_barang` DISABLE KEYS */;
 INSERT INTO `data_barang` (`kode_barang`, `nama_barang`, `harga_barang`) VALUES
-	('1', 'ASUS ROG', 30000000),
-	('2', 'RTX 3000 Series', 5000000),
-	('B001', 'Aqua', 700);
+	('B001', 'Asus ROG ', 30000000),
+	('B002', 'Aqua', 2500);
 /*!40000 ALTER TABLE `data_barang` ENABLE KEYS */;
 
 -- Dumping structure for table admin_smartvillage.data_layanan
 CREATE TABLE IF NOT EXISTS `data_layanan` (
-  `kode_layanan` char(5) COLLATE utf8_bin NOT NULL,
+  `kode_layanan` char(10) COLLATE utf8_bin NOT NULL,
   `nama_layanan` varchar(30) COLLATE utf8_bin NOT NULL,
-  `harga_layanan` int(11) NOT NULL,
+  `harga_layanan` int(11) unsigned NOT NULL,
   PRIMARY KEY (`kode_layanan`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -47,8 +67,8 @@ CREATE TABLE IF NOT EXISTS `data_layanan` (
 DELETE FROM `data_layanan`;
 /*!40000 ALTER TABLE `data_layanan` DISABLE KEYS */;
 INSERT INTO `data_layanan` (`kode_layanan`, `nama_layanan`, `harga_layanan`) VALUES
-	('L01', 'Web OpenSID', 10000000),
-	('L02', 'Input Data', 3000000);
+	('L001', 'Web OpenSID', 10000000),
+	('L002', 'Input Data', 3000000);
 /*!40000 ALTER TABLE `data_layanan` ENABLE KEYS */;
 
 -- Dumping structure for table admin_smartvillage.data_pelanggan
@@ -74,31 +94,41 @@ INSERT INTO `data_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `nama_desa`, `no
 CREATE TABLE IF NOT EXISTS `data_transaksi` (
   `id_transaksi` varchar(64) COLLATE utf8_bin NOT NULL,
   `id_pelanggan` char(64) COLLATE utf8_bin NOT NULL,
-  `kode_barang` char(5) COLLATE utf8_bin DEFAULT NULL,
-  `qty` int(11) DEFAULT NULL,
-  `kode_layanan` char(5) COLLATE utf8_bin DEFAULT NULL,
   `total` bigint(20) DEFAULT NULL,
   `tanggal` date NOT NULL,
   `status` tinyint(4) NOT NULL,
   PRIMARY KEY (`id_transaksi`),
   KEY `id_pelanggan` (`id_pelanggan`),
-  KEY `FK_data_transaksi_data_barang` (`kode_barang`),
-  KEY `FK_data_transaksi_data_layanan` (`kode_layanan`),
-  CONSTRAINT `FK_data_transaksi_data_barang` FOREIGN KEY (`kode_barang`) REFERENCES `data_barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_data_transaksi_data_layanan` FOREIGN KEY (`kode_layanan`) REFERENCES `data_layanan` (`kode_layanan`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `data_transaksi_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `data_pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table admin_smartvillage.data_transaksi: ~5 rows (approximately)
+-- Dumping data for table admin_smartvillage.data_transaksi: ~1 rows (approximately)
 DELETE FROM `data_transaksi`;
 /*!40000 ALTER TABLE `data_transaksi` DISABLE KEYS */;
-INSERT INTO `data_transaksi` (`id_transaksi`, `id_pelanggan`, `kode_barang`, `qty`, `kode_layanan`, `total`, `tanggal`, `status`) VALUES
-	('62a4a3ae21f87', '2', '2', 3, NULL, 15000000, '2022-06-11', 0),
-	('62a4a3e55ef85', '1', '2', 2, 'L02', 13000000, '2022-06-11', 1),
-	('62a4a42ab7c63', '1', '1', 1, 'L01', 40000000, '2022-06-11', 1),
-	('62a6659c80786', '2', NULL, 0, 'L02', 3000000, '2022-06-11', 0),
-	('62abedc40d60b', '1', 'B001', 3, NULL, 2100, '2022-06-17', 1);
+INSERT INTO `data_transaksi` (`id_transaksi`, `id_pelanggan`, `total`, `tanggal`, `status`) VALUES
+	('62afed9b1459a', '1', 33000000, '2022-06-20', 1),
+	('62afeebf68cfe', '2', 10005000, '2022-06-20', 1);
 /*!40000 ALTER TABLE `data_transaksi` ENABLE KEYS */;
+
+-- Dumping structure for table admin_smartvillage.layanan_order
+CREATE TABLE IF NOT EXISTS `layanan_order` (
+  `id_layanan` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `kode_layanan` char(11) COLLATE utf8_bin DEFAULT NULL,
+  `nama_layanan` mediumtext COLLATE utf8_bin,
+  `harga_layanan` int(10) unsigned DEFAULT '0',
+  `id_transaksi` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_layanan`),
+  KEY `FK__data_transaksi_layanan` (`id_transaksi`),
+  CONSTRAINT `FK__data_transaksi_layanan` FOREIGN KEY (`id_transaksi`) REFERENCES `data_transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumping data for table admin_smartvillage.layanan_order: ~1 rows (approximately)
+DELETE FROM `layanan_order`;
+/*!40000 ALTER TABLE `layanan_order` DISABLE KEYS */;
+INSERT INTO `layanan_order` (`id_layanan`, `kode_layanan`, `nama_layanan`, `harga_layanan`, `id_transaksi`) VALUES
+	('62afed9b23437', 'L002', 'Input Data', 3000000, '62afed9b1459a'),
+	('62afeebf6a378', 'L001', 'Web OpenSID', 10000000, '62afeebf68cfe');
+/*!40000 ALTER TABLE `layanan_order` ENABLE KEYS */;
 
 -- Dumping structure for table admin_smartvillage.perjanjian_kerjasama
 CREATE TABLE IF NOT EXISTS `perjanjian_kerjasama` (
@@ -116,7 +146,8 @@ CREATE TABLE IF NOT EXISTS `perjanjian_kerjasama` (
 DELETE FROM `perjanjian_kerjasama`;
 /*!40000 ALTER TABLE `perjanjian_kerjasama` DISABLE KEYS */;
 INSERT INTO `perjanjian_kerjasama` (`id_pks`, `nama_desa`, `nama_kades`, `tanggal`, `id_transaksi`) VALUES
-	('62abedc41ae45', 'Pringsewu', '', '2022-06-17', '62abedc40d60b');
+	('62afed9b23a7d', 'Pringsewu', '', '2022-06-20', '62afed9b1459a'),
+	('62afeebfa82f4', 'Sekampung', '', '2022-06-20', '62afeebf68cfe');
 /*!40000 ALTER TABLE `perjanjian_kerjasama` ENABLE KEYS */;
 
 -- Dumping structure for table admin_smartvillage.users
@@ -134,12 +165,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table admin_smartvillage.users: ~1 rows (approximately)
+-- Dumping data for table admin_smartvillage.users: ~2 rows (approximately)
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id_user`, `username`, `password`, `nama_lengkap`, `jabatan`, `role_id`, `email`, `no_telp`, `alamat`) VALUES
-	('1', 'ahmadsyafar', '$2a$12$PMNuf2cbjHMrrvo27U/rO.Bq75DF33X2ll6k2Pm4sxnRd6Eef7HJa', 'Ahmad Syafarudin', 0, 0, 'ahmad@magang.com', '0895604395176', 'Pringsewu'),
-	('62a19deb7cc67', 'admin', '$2a$12$PMNuf2cbjHMrrvo27U/rO.Bq75DF33X2ll6k2Pm4sxnRd6Eef7HJa', '', 0, 2, '', '', '');
+	('62a19deb7cc67', 'admin', '$2a$12$twOWVeXBj42RVi8SKZlIDuLlJqzhnjVI4A8Lhjj3.0Ih0ak6QWbaW', '', 0, 321, '', '', ''),
+	('62ad51525f56b', 'ahmadsyafar', '$2y$10$Me37Xf9YSxVqcj5.6beqnOmL85.5F1aHh4LTGNagsQyAzBSgFe/gK', '', 0, 2, '', '', '');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
